df0ed552626fbbcd9325208d6bc1ef12f7860821
shebna12 <shebna12@gmail.com>
2018-04-19 02:07:20 +0800

    fitness func to be checked by adviser

--- fn/fitness.py
+++ fn/fitness.py
@@ -51,6 +51,7 @@ def knapsack_fitness(state,feasibility_minimum):
 		return score
 
 def vertex_cover_fitness(state,feasibility_minimum):
+	# print("--------------------------The beginning of Vertex Cover!-----------------------------")
 	problem = state.problem
 	solution = state.solution
 
@@ -58,15 +59,55 @@ def vertex_cover_fitness(state,feasibility_minimum):
 		# has violation: some edges are not covered
 		max_score = int(feasibility_minimum * 0.75)
 		used_vertices = [v for v in problem.variables if solution[v] == 1]
+		
 
 		# INSERT CODE HERE
 		# Idea: less uncovered edges = higher score
 		# Hint: use problem.edges, used_vertices
+
+		problem_edges_length = len(problem.edges)
+		covered_edges = 0
+
+		# Check if that edge contains a vertex from the used_vertices
+		for edge in problem.edges:
+			if (edge[0] in used_vertices or edge[1] in used_vertices):
+				covered_edges = covered_edges + 1
+
+		score = (covered_edges/problem_edges_length)*100
+
+		# If the score is beyond the max_score, lower it down to the max score level
+		if(score > max_score):
+			score = max_score
+		
+		# print("Score: ",score)
+
+
+
+
 	else:	
+	
 		# no violations
 		score = feasibility_minimum # min score for being valid solution
 
 		# INSERT CODE HERE
 		# Idea: less vertices used = better
 		# So, higher no. of unused vertices in solution = higher score
+
+		used_vertices = [v for v in problem.variables if solution[v] == 1]
+
+		# Get the opposite of used_vertices to fulfill the idea of "less vertices = better" scoring.
+		used_vertices_length = len(used_vertices_length)
+		problem_vertices_length = len(problem.variables)
+		unused_vertices_length = problem_vertices_length - used_vertices_length
+		computed_score = (unused_vertices_length/problem_vertices_length) * 100
+
+		# If the computed score is less than the assumed min score, return the computed score 
+		if(computed_score < score):
+			score = computed_score
+		
+		# print("Purrfect score:", score)
+
+
+	return score
+			
 
